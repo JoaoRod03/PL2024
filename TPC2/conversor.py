e78e82f75874fd0997f5cd6c6fe8ccc49b304cd5
João Rodrigues <jprocharodrigues2003@gmail.com>
2024-02-17 14:47:47 +0000

    pequenos ajustes

--- TPC2/conversor.py
+++ TPC2/conversor.py
@@ -2,12 +2,11 @@ import re
 
 with open("input.md", "r", encoding="utf-8") as md:
     text = md.read()
-
-    
+ 
 # Cabeçalhos
-text = re.sub(r'^(#)\s+(.+)$', r'<h1>\2</h1>', text, flags=re.MULTILINE)
-text = re.sub(r'^(##)\s+(.+)$', r'<h2>\2</h2>', text, flags=re.MULTILINE)
-text = re.sub(r'^(###)\s+(.+)$', r'<h3>\2</h3>', text, flags=re.MULTILINE)
+text = re.sub(r'^(#)\s+(.+)$', r'<h1>\2</h1>', text, flags = re.MULTILINE)
+text = re.sub(r'^(##)\s+(.+)$', r'<h2>\2</h2>', text, flags = re.MULTILINE)
+text = re.sub(r'^(###)\s+(.+)$', r'<h3>\2</h3>', text, flags = re.MULTILINE)
     
 # Bold
 text = re.sub(r'\*\*(.+?)\*\*', r'<b>\1</b>', text)
@@ -17,7 +16,7 @@ text = re.sub(r'\_\_(.+?)\_\_', r'<b>\1</b>', text)
 text = re.sub(r'\*(.+?)\*', r'<i>\1</i>', text)
     
 # Lista numerada
-text = re.sub(r'^(?=\d+\. )(.+)$', r'<li>\1</li>', text, flags=re.MULTILINE)
+text = re.sub(r'^(?=\d+\. )(.+)$', r'<li>\1</li>', text, flags = re.MULTILINE)
 text = re.sub(r'((<li>.+\n)+)', r'<ol>\1</ol>', text)
     
 # Imagem
@@ -28,4 +27,3 @@ text = re.sub(r'\[(.+?)\]\((.+?)\)', r'<a href="\2">\1</a>', text)
     
 with open("index.html", "w") as f:
     f.write(text)
-
